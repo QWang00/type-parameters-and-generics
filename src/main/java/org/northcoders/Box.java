1ca40afbe1ed9591bb78ab93efb26f3c6125d60a
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-07-26 12:25:38 +0100

    feat: complete hasSameName method

--- src/main/java/org/northcoders/Box.java
+++ src/main/java/org/northcoders/Box.java
@@ -1,10 +1,43 @@
 package org.northcoders;
 
-import com.sun.jdi.event.ExceptionEvent;
-
-public class Box<T> {
+public class Box<T, V> {
 
     private T number;
+    private String name;
+    private V largestValue;
+
+    public Box(String name) {
+        this.name = name;
+    }
+
+    public V getLargestValue() {
+        return largestValue;
+    }
+
+    public void setLargestValue(V <? extends Number> firstValue, V <? extends Number>secondValue) {
+        Number largest = (Number) firstValue;
+        Number newSecond = (Number) secondValue;
+        if(newSecond > largest){
+            largest = (Number) secondValue;
+        }
+        this.largestValue = (V) largest;
+      }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean hasSameName(Box <? extends T, V> box ){
+        // check if box name is same as this
+        if(this.getName().equals(box.getName())){
+            return true;
+        }
+        return false;
+    }
 
     public T getNumber() {
         return number;
--- src/main/java/org/northcoders/Main.java
+++ src/main/java/org/northcoders/Main.java
@@ -1,11 +1,27 @@
 package org.northcoders;
 
+
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Box<String> box = new Box<>();
-        box.castValue(5);
+        Box<String, Number> box1 = new Box<>("Str");
+        Box<Number, String> box2 = new Box<>("Num");
+        Box<Integer, String> box22 = new Box<>("Int");
+        Box<Double, String> box23 = new Box<>("Num");
+        // whatever passed is the child of "this"
+        boolean result = box2.hasSameName(box22);
+        boolean result2 = box2.hasSameName(box23);
+
+
+        System.out.println("compare Number and Integer name is " +result);
+        System.out.println("compare Number and Double name is " +result2);
+
+
+        //ArrayList<Integer> nums = {1,2,3};
+        box1.castValue(5);
 
 
 
