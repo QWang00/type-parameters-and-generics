920f0a43a05872afcd1381651684e982e71706f6
dimadeloseros1 <dimadeloseros1@gmail.com>
2024-07-26 10:57:02 +0100

    Feat: Creating a Generic Class completed

---
+++ src/main/java/org/northcoders/Box.java
@@ -0,0 +1,14 @@
+package org.northcoders;
+
+public class Box<T> {
+
+    private T number;
+
+    public T getNumber() {
+        return number;
+    }
+
+    public void setNumber(T number) {
+        this.number = number;
+    }
+}
